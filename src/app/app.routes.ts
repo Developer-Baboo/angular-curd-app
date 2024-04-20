5734ef4f2113fb1d30fa7ca9e0417a2584e8d0ed
Developer-Baboo <babooheerani@gmail.com>
2024-04-20 12:30:50 +0500

    add components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { ContactPageComponent } from './Pages/contact-page/contact-page.component';
+import { AboutPageComponent } from './Pages/about-page/about-page.component';
+import { NavbarComponent } from './Pages/Partials/navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomePageComponent, title: 'Home Page'},
+    { path: 'contact-page', component: ContactPageComponent, title: 'Contact Page'},
+    { path: 'about-page', component: AboutPageComponent, title: 'About Page'},
+    { path: 'navbar', component: NavbarComponent, title: 'Nav Bar'}
+];
