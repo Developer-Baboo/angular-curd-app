9dd56d044aa8c3b76194a2db87db7a00d54a9512
Developer-Baboo <babooheerani@gmail.com>
2024-04-25 23:53:33 +0500

    updated

--- src/app/Pages/Partials/navbar/navbar.component.ts
+++ src/app/Pages/Partials/navbar/navbar.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
 
+export class NavbarComponent {
+  activeLink: string = '';
+
+  constructor(private router: Router) {}
+
+  setActiveLink(link: string) {
+    this.activeLink = link;
+  }
 }
